72e72ca5f15a861f2c11f146b2e4b68ab084cadd
pieecia <piotr.kananowicz@icloud.com>
2025-11-06 19:58:51 +0100

    fix: missing package

--- config/shadcn.tailwind.js
+++ config/shadcn.tailwind.js
@@ -92,7 +92,6 @@ module.exports = {
         require("@tailwindcss/forms"),
         require("@tailwindcss/aspect-ratio"),
         require("@tailwindcss/typography"),
-        require("@tailwindcss/container-queries"),
-        require("tailwindcss-animate")
+        require("@tailwindcss/container-queries")
     ],
 };
