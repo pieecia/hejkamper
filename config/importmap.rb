5ab4cecf93e1fa2b88977335de227888b2144a3e
pieecia <piotr.kananowicz@icloud.com>
2025-11-07 11:01:38 +0100

    fix: clean up tailwind animations

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,6 +7,3 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "hotkeys-js", to: "https://cdn.jsdelivr.net/npm/hotkeys-js@3.13.7/dist/hotkeys.esm.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "leaflet" # @1.9.4
-pin "tailwindcss-animated" # @2.0.0
-pin "tailwindcss/package.json", to: "tailwindcss--package.json.js" # @4.1.16
-pin "tailwindcss/plugin", to: "tailwindcss--plugin.js" # @4.1.16
--- config/shadcn.tailwind.js
+++ config/shadcn.tailwind.js
@@ -92,6 +92,7 @@ module.exports = {
         require("@tailwindcss/forms"),
         require("@tailwindcss/aspect-ratio"),
         require("@tailwindcss/typography"),
-        require("@tailwindcss/container-queries")
+        require("@tailwindcss/container-queries"),
+        require("tailwindcss-animate")
     ],
 };
